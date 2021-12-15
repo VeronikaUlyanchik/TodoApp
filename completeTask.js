21dd8953187ef43df840624b358e4b3b8ebc447b
Veronika Ulyanchik <84153492+VeronikaUlyanchik@users.noreply.github.com>
2021-12-16 02:48:46 +0300

    Revert "app: with fetch and scss"

--- completeTask.js
+++ completeTask.js
@@ -1,27 +1,20 @@
-async function completeTask(id) {
-    const completedObj= tasks.filter(item => item.id ===id)
-    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            userId: idUser,
-            id: id,
-            title: completedObj[0].title,
-            completed: true,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      let newTask = await response.json();
+function completeTask(id) {
+    const completedObj= JSON.parse(localStorage.getItem('tasks')).filter(item => item.id ===id)
+    const newTask = {
+        text: completedObj[0].text,
+        id: id,
+        checked: true,
+    };
     completedTask.push(newTask);
+    localStorage.setItem('completedtasks', JSON.stringify(completedTask));
     generateTask(newTask);
     deleteTodo(id);
     amountTasks();
 }
 
 function uncompleteTask(id) {
-    const uncompletedObj= completedTask.filter(item => item.id ===id)
-    let text = uncompletedObj[0].title;
+    const uncompletedObj= JSON.parse(localStorage.getItem('completedtasks')).filter(item => item.id ===id)
+    let text = uncompletedObj[0].text;
     addItem(text);
     deleteCompleted(id);
     amountTasks();
