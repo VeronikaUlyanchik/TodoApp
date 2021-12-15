0a79bbf16f39d1b229486237001d89b9994ef5e8
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2021-12-16 02:40:29 +0300

    app: with fetch and scss

--- editTask.js
+++ editTask.js
@@ -1,10 +1,7 @@
-let indexForEdit;
-const editBtn = document.querySelector('#btn_edit_task');
-
 function editTask(id) {
     const editObj= tasks.filter(item => item.id ===id);
     const indexEdit = tasks.findIndex(item => item.id ===id);
-    inputTask.value = editObj[0].text;
+    inputTask.value = editObj[0].title;
     editBtn.style.display = 'block';
     addBtn.style.display = 'none';
     addBtn.setAttribute("disabled", "disabled");
@@ -12,24 +9,37 @@ function editTask(id) {
     indexForEdit=indexEdit;
 }
 
-
 editBtn.addEventListener('click', editTaskShow) 
 
-
-function editTaskShow(){
+async function editTaskShow(){
     
-    tasks[indexForEdit].text=inputTask.value;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    tasks[indexForEdit].title=inputTask.value;
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${tasks[indexForEdit].id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            userId: idUser,
+            id: tasks[indexForEdit].id,
+            title: tasks[indexForEdit].title,
+            completed: true,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
     todoField.innerHTML="";
     tasks.forEach(t => {
         generateTask(t);
     })
+    returnToPrev()
+}
+
+function returnToPrev(){
     inputTask.value = "";
     editBtn.style.display = 'none';
     addBtn.style.display = 'inline';
+    indexForEdit=undefined;
     addBtn.removeAttribute("disabled", "disabled");
     amountTasks();
 }
-
 
 
